77059900196f468d56baaf769ade3913c38304cb
tomaszpankowski <--get-all>
2022-05-18 17:12:32 +0200

    frontend update

--- src/components/investment-s2.js
+++ src/components/investment-s2.js
@@ -7,18 +7,18 @@ import Button from "../../node_modules/react-bootstrap/Button";
 class InvestmentS2 extends Component{
     render(){
         return(    
-            <Container fluid className="d-flex minh-50vh bg-light align-items-center py-5 border-top border-secondary pt-5">
+            <Container fluid className="investment-s2 d-flex minh-50vh bg-light align-items-center py-5 border-top border-secondary pt-5">
             <Row className="mx-0 w-100 mt-5">
-                <Col xs={{span:12,order:1}} md={{span:7,order:2}} className="minh-25vh"></Col>
-                <Col xs={{span:12,order:2}} md={{span:5,order:1}} className="minh-50vh d-flex align-items-center">
+                <Col xs={{span:12,order:1}} md={{span:8,order:2}} className="minh-25vh"></Col>
+                <Col xs={{span:12,order:2}} md={{span:4,order:1}} className="minh-50vh d-flex align-items-center">
                     <div className="w-100 text-center text-md-left">
-                        <h5 className="text-dark">
+                        <h5 className="text-white">
                             Sed ut perspiciatis
                         </h5>
-                        <h2 className="dispaly-6 fw-bold text-secondary">
+                        <h2 className="dispaly-6 fw-bold text-white">
                             Unde omnis iste
                         </h2>
-                        <p className="text-muted">
+                        <p className="text-white">
                             Natus error sit voluptatem accusantium doloremque laudantium, 
                             totam rem aperiam, eaque ipsa quae ab illo inventore veritatis 
                             et quasi architecto beatae vitae dicta sunt explicabo. Nemo 
